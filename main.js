17aa0fc8716db51650054ce3c3591c85c9520613
agustin-pasten <aopasten@miuandes.cl>
2021-05-02 12:18:14 -0400

    commit final

---
+++ main.js
@@ -0,0 +1,17 @@
+function fibonacci1(a){
+    console.log(a);
+    add= a+a;
+    console.log(add);
+    add2 = add +a;
+    console.log(add2);
+    add3 = add + add2
+    console.log(add3);
+    add = add2 + add3;
+    console.log(add);
+}
+
+console.log("this code works in chrome terminal")
+let number1 = prompt("put a number: ");
+var number =parseInt(number1);
+console.log("the secuence of Fibonacci are:");
+fibonacci1(number);
